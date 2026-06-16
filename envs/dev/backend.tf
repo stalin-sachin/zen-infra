67cce402607c6a3072e986956c0e6c2122567132
stalin-sachin <141940525+stalin-sachin@users.noreply.github.com>
2026-06-16 10:56:10 +0530

    Change S3 backend region from us-east-1 to ap-south-1

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "zen-pharma-terraform-state-ravdy"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
-    region       = "us-east-1"
+    region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true   # S3 native locking
   }
