bd72f2cc3bc94d95c3856c16c6f88e4a6a620838
stalin-sachin <141940525+stalin-sachin@users.noreply.github.com>
2026-06-16 11:11:57 +0530

    Change default AWS region in setup script
    Updated default AWS region from 'us-east-1' to 'ap-south-1'.

--- scripts/03-setup-external-secrets.sh
+++ scripts/03-setup-external-secrets.sh
@@ -114,8 +114,8 @@ prompt_choice ENV \
 
 prompt AWS_REGION \
   "AWS region where your Secrets Manager secrets are stored" \
-  "us-east-1" \
-  "us-east-1"
+  "ap-south-1" \
+  "ap-south-1"
 
 prompt AWS_ACCOUNT_ID \
   "AWS account ID (12-digit number - find it in the top-right of the AWS console)" \
